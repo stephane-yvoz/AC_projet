9d201ef06b7f52b22ae56ded1d30960777af6af3
yvoz1u <stephane.yvoz6@etu.univ-lorraine.fr>
2018-12-27 11:17:21 +0100

    commit initial

---
+++ src/graphe/Edge.java
@@ -0,0 +1,18 @@
+package src.graphe;
+class Edge
+{
+   int from;
+   int to;
+   boolean used;
+    Edge(int x, int y)
+    {
+	this.from = x;
+	this.to = y;
+	this.used = false;
+    }
+    
+    final int other(int v)
+    {
+	if (this.from == v) return this.to; else return this.from;
+    }    
+}
