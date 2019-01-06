3d70b624411547ab28764ce65431be73aaea3f37
yvoz1u <stephane.yvoz6@etu.univ-lorraine.fr>
2019-01-06 22:55:12 +0100

    nette amélioration, mais pas assez

--- src/partie2/dynamique.java
+++ src/partie2/dynamique.java
@@ -3,6 +3,9 @@ package src.partie2;
 public class dynamique {
 	
 	public static int nbCombinaisons(int k,int n, int b,int m) {
+		if(k<n) {
+			return 0;
+		}
 		if(n==0) {
 			return 1;
 		}
@@ -17,13 +20,13 @@ public class dynamique {
 				return factorielle(m-1);
 			}
 			return (m-1)* nbCombinaisons(k-1,n-1,b,m-1) + (k-n) * nbCombinaisons(k-1,n-1,b,m) +
-					 m *nbCombinaisons(k-1,n-1,b,m-1)+ (k-n)*nbCombinaisons(k-1,n-1,b,m);
+					 m *nbCombinaisons(k-2,n-1,b,m-1)+ (k-n)*nbCombinaisons(k-2,n-1,b,m);
 		}
 		if(n==(b+m)) { // le premier élément ne peux pas être faux
 			return nbCombinaisons(k-1,n-1,b-1,m) +(m-1)* nbCombinaisons(k-1,n-1,b,m-1) + (k-n) * nbCombinaisons(k-1,n-1,b,m);
 		}
 		return nbCombinaisons(k-1,n-1,b-1,m) +(m-1)* nbCombinaisons(k-1,n-1,b,m-1) + (k-n) * nbCombinaisons(k-1,n-1,b,m)
-				+  m *nbCombinaisons(k-1,n-1,b,m-1)+ (k-n)*nbCombinaisons(k-1,n-1,b,m); 
+				+  m *nbCombinaisons(k-2,n-1,b,m-1)+ (k-n)*nbCombinaisons(k-2,n-1,b,m); 
 	}
 
 	
